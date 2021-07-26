e3af0590979082253cacf1e3e6fc22a88b473b68
Donnell Albertie <dalbertie5@gmail.com>
2021-07-26 00:05:30 -0500

    ProductManager

---
+++ src/views/Main.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import ProductForm from '../components/ProductForm';
+import axios from 'axios';
+
+export default () => {
+
+
+    return (
+        <ProductForm/>
+    )
+}
